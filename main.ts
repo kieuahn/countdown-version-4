9dbddfcf7ef065bd77f4036b88b625092b233aa3
kieuahn <73641824+kieuahn@users.noreply.github.com>
2021-11-30 17:15:41 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onPinPressed(TouchPin.P0, function () {
+    if (set_Timerminutes < 59) {
+        basic.showNumber(set_Timerminutes)
+        set_Timerminutes += 10
+    } else {
+        set_Timerminutes = 0
+    }
+})
 input.onButtonPressed(Button.A, function () {
     if (set_Timerhours <= 12) {
         basic.showNumber(set_Timerhours)
@@ -27,19 +35,18 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
     while (set_Timerseconds > 0) {
         set_Timerseconds += 0 - 1
-        basic.showNumber(set_Timerseconds)
-        basic.pause(100)
-        basic.showString("" + set_Timerminutes + ":" + set_Timerseconds)
         if (set_Timerseconds == 0) {
             set_Timerminutes += 0 - 1
             set_Timerseconds = 59
-            basic.showString("" + set_Timerminutes + ":" + set_Timerseconds)
             if (set_Timerminutes == 0) {
                 set_Timerhours += 0 - 1
                 set_Timerminutes = 59
                 basic.showString("" + set_Timerhours + ":" + set_Timerminutes + ":" + set_Timerseconds)
             }
+            basic.showString("" + set_Timerminutes + ":" + set_Timerseconds)
         }
+        basic.showNumber(set_Timerseconds)
+        basic.pause(100)
     }
 })
 let set_Timerseconds = 0
