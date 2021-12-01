36b14a4632a73e5b7e155a9ad4f24584692fba1a
kieuahn <73641824+kieuahn@users.noreply.github.com>
2021-12-01 16:13:33 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def on_pin_pressed_p0():
-    global set_Timerminutes
-    if set_Timerminutes < 59:
-        basic.show_number(set_Timerminutes)
-        set_Timerminutes += 10
-    else:
-        set_Timerminutes = 0
-input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
-
 def on_button_pressed_a():
     global set_Timerhours
     if set_Timerhours <= 12:
@@ -17,6 +8,14 @@ def on_button_pressed_a():
     else:
         set_Timerhours = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_pin_pressed_p2():
+    music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    basic.show_string("" + str(hour) + ":" + str(min2) + ":" + str(second))
+    basic.show_string("" + str((input.temperature())))
+    basic.show_string("" + str((input.light_level())))
+    basic.pause(1000)
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
 
 def on_button_pressed_ab():
     global set_Timerseconds
@@ -36,15 +35,22 @@ def on_button_pressed_b():
         set_Timerminutes = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def on_pin_pressed_p1():
+    pass
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
 def on_gesture_shake():
     global set_Timerseconds, set_Timerminutes, set_Timerhours
     while set_Timerseconds > 0:
         set_Timerseconds += 0 - 1
+        music.play_tone(262, music.beat(BeatFraction.QUARTER))
         if set_Timerseconds == 0:
             set_Timerminutes += 0 - 1
+            music.play_tone(262, music.beat(BeatFraction.QUARTER))
             set_Timerseconds = 59
             if set_Timerminutes == 0:
                 set_Timerhours += 0 - 1
+                music.play_tone(262, music.beat(BeatFraction.QUARTER))
                 set_Timerminutes = 59
                 basic.show_string("" + str(set_Timerhours) + ":" + str(set_Timerminutes) + ":" + str(set_Timerseconds))
             basic.show_string("" + str(set_Timerminutes) + ":" + str(set_Timerseconds))
@@ -53,11 +59,26 @@ def on_gesture_shake():
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 set_Timerseconds = 0
+second = 0
+min2 = 0
+hour = 0
 set_Timerminutes = 0
 set_Timerhours = 0
 set_Timerhours = 0
 set_Timerminutes = 0
+hour = 12
+min2 = 59
+second = 50
 
 def on_forever():
-    pass
+    global second, min2, hour
+    second += 1
+    if second >= 60:
+        second = 0
+        min2 += 1
+    if min2 >= 60:
+        min2 = 0
+        hour += 1
+    if hour >= 24:
+        hour = 0
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
-input.onPinPressed(TouchPin.P0, function () {
-    if (set_Timerminutes < 59) {
-        basic.showNumber(set_Timerminutes)
-        set_Timerminutes += 10
-    } else {
-        set_Timerminutes = 0
-    }
-})
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (set_Timerhours <= 12) {
         basic.showNumber(set_Timerhours)
         set_Timerhours += 1
@@ -15,24 +8,40 @@ input.onButtonPressed(Button.A, function () {
     } else {
         set_Timerhours = 0
     }
+    
 })
-input.onButtonPressed(Button.AB, function () {
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showString("" + ("" + hour) + ":" + ("" + min2) + ":" + ("" + second))
+    basic.showString("" + ("" + input.temperature()))
+    basic.showString("" + ("" + input.lightLevel()))
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     if (set_Timerseconds < 59) {
         basic.showNumber(set_Timerseconds)
         set_Timerseconds += 1
     } else {
         set_Timerseconds = 0
     }
+    
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (set_Timerminutes < 59) {
         basic.showNumber(set_Timerminutes)
         set_Timerminutes += 1
     } else {
         set_Timerminutes = 0
     }
+    
 })
-input.onGesture(Gesture.Shake, function () {
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
     while (set_Timerseconds > 0) {
         set_Timerseconds += 0 - 1
         music.playTone(262, music.beat(BeatFraction.Quarter))
@@ -44,16 +53,42 @@ input.onGesture(Gesture.Shake, function () {
                 set_Timerhours += 0 - 1
                 music.playTone(262, music.beat(BeatFraction.Quarter))
                 set_Timerminutes = 59
-                basic.showString("" + set_Timerhours + ":" + ("" + set_Timerminutes) + ":" + ("" + set_Timerseconds))
+                basic.showString("" + ("" + set_Timerhours) + ":" + ("" + set_Timerminutes) + ":" + ("" + set_Timerseconds))
             }
-            basic.showString("" + set_Timerminutes + ":" + ("" + set_Timerseconds))
+            
+            basic.showString("" + ("" + set_Timerminutes) + ":" + ("" + set_Timerseconds))
         }
+        
         basic.showNumber(set_Timerseconds)
         basic.pause(100)
     }
 })
 let set_Timerseconds = 0
+let second = 0
+let min2 = 0
+let hour = 0
 let set_Timerminutes = 0
 let set_Timerhours = 0
 set_Timerhours = 0
 set_Timerminutes = 0
+hour = 12
+min2 = 59
+second = 50
+basic.forever(function on_forever() {
+    
+    second += 1
+    if (second >= 60) {
+        second = 0
+        min2 += 1
+    }
+    
+    if (min2 >= 60) {
+        min2 = 0
+        hour += 1
+    }
+    
+    if (hour >= 24) {
+        hour = 0
+    }
+    
+})
